2c75a9f51bd97e507ec69497cbe9455a88fe3aa3
lichang <changli@alauda.io>
2019-07-28 13:03:02 +0800

    test2

--- controllers/node.go
+++ controllers/node.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"meter-panel/store"
 	"net/http"
@@ -11,9 +12,11 @@ import (
 func NodeViews(w http.ResponseWriter, r *http.Request) {
 	cluster := r.FormValue("cluster")
 	tmp := store.StoreAllClusterNodeList[cluster]
+	fmt.Println(tmp)
 	data2, err := json.Marshal(tmp)
 	if err != nil {
 		log.Println(err)
 	}
+	fmt.Println(data2)
 	w.Write(data2)
 }
