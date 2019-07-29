b0c86f813de7ece835372dc48f6b48c6281b431e
lichang <changli@alauda.io>
2019-07-29 15:02:59 +0800

    test16

---
+++ store/init.go
@@ -0,0 +1,54 @@
+package store
+
+import (
+	"log"
+	"meter-panel/configs"
+	"meter-panel/pkg/api/v1/k8s"
+)
+
+var (
+	//StoreAllClusterNodeList is used save all cluster node list in the memory
+	StoreAllClusterNodeList k8s.HumanAllClusterNodeList
+	// StoreAllK8SConfigs is used save all cluster configs in the memory
+	StoreAllK8SConfigs configs.HumanAllK8SConfigs
+	// NilSingleClusterNodeList is used return nil value of HumanSingleClusterNodeList
+	NilSingleClusterNodeList k8s.HumanSingleClusterNodeList
+	err                      error
+)
+
+func init() {
+	StoreAllK8SConfigs, err = configs.GetK8SCoinfg()
+	if err != nil {
+		log.Println(err)
+	}
+
+	StoreAllClusterNodeList, err = k8s.ListAllClusterNodes(StoreAllK8SConfigs)
+	if err != nil {
+		log.Println(err)
+	}
+
+	k8s.WatchAllClusterNodes(StoreAllK8SConfigs)
+}
+
+func init() {
+	nodedetail := make(map[string]string)
+	go func() {
+		for {
+			tmp := <-k8s.K8sRequest.Chan
+			for k, v := range tmp {
+				// k was cluster name
+				// 处理序列化后的数据
+				switch x := v.(type) {
+				case k8s.NodeEvents:
+					nodename := x.Object.Metadata.Name
+					for _, v2 := range x.Object.Status.Conditions {
+						if v2.Type == "Ready" {
+							nodedetail[nodename] = v2.Status
+						}
+					}
+					NodeModifyed(k, nodedetail, nodename)
+				}
+			}
+		}
+	}()
+}
--- store/nodecache.go
+++ store/nodecache.go
@@ -2,57 +2,9 @@ package store
 
 import (
 	"log"
-	"meter-panel/configs"
 	k8s "meter-panel/pkg/api/v1/k8s"
 	"reflect"
 )
-
-var (
-	//StoreAllClusterNodeList is used save all cluster node list in the memory
-	StoreAllClusterNodeList k8s.HumanAllClusterNodeList
-	// StoreAllK8SConfigs is used save all cluster configs in the memory
-	StoreAllK8SConfigs configs.HumanAllK8SConfigs
-	// NilSingleClusterNodeList is used return nil value of HumanSingleClusterNodeList
-	NilSingleClusterNodeList k8s.HumanSingleClusterNodeList
-	err                      error
-)
-
-func init() {
-	StoreAllK8SConfigs, err = configs.GetK8SCoinfg()
-	if err != nil {
-		log.Println(err)
-	}
-
-	StoreAllClusterNodeList, err = k8s.ListAllClusterNodes(StoreAllK8SConfigs)
-	if err != nil {
-		log.Println(err)
-	}
-
-	k8s.WatchAllClusterNodes(StoreAllK8SConfigs)
-}
-
-func init() {
-	nodedetail := make(map[string]string)
-	go func() {
-		for {
-			tmp := <-k8s.K8sRequest.Chan
-			for k, v := range tmp {
-				// k was cluster name
-				// 处理序列化后的数据
-				switch x := v.(type) {
-				case k8s.NodeEvents:
-					nodename := x.Object.Metadata.Name
-					for _, v2 := range x.Object.Status.Conditions {
-						if v2.Type == "Ready" {
-							nodedetail[nodename] = v2.Status
-						}
-					}
-					NodeModifyed(k, nodedetail, nodename)
-				}
-			}
-		}
-	}()
-}
 
 // GetSingleClusterNodeList is used query node info of a cluster
 // when a new cluster create in furion, this function can refresh the info of cluster
@@ -71,7 +23,7 @@ func GetSingleClusterNodeList(cluster string) k8s.HumanSingleClusterNodeList {
 	return tmp
 }
 
-// Modifyed is used when node event type is Modifyed
+// NodeModifyed is used when node event type is Modifyed
 func NodeModifyed(cluster string, nodedetail map[string]string, nodename string) {
 	if StoreAllClusterNodeList[cluster].SingleClusterHealthyNode.NodeStatus[nodename] == "" {
 		if StoreAllClusterNodeList[cluster].SingleClusterUnHealthyNode.NodeStatus[nodename] == nodedetail[nodename] {
