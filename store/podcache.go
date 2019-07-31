cde05271b377615092a59aed41a648d83975ecdc
lichang <changli@alauda.io>
2019-07-31 10:48:36 +0800

    fix watch events can't do some something

--- store/podcache.go
+++ store/podcache.go
@@ -29,10 +29,12 @@ func PodModifyed(cluster string, poddetail map[string]k8s.Pod, podname string, e
 		if StoreAllClusterPodList[cluster].SingleClusterHealthyPods.PodStatus[podname] == NilK8SPod {
 			delete(StoreAllClusterPodList[cluster].SingleClusterHealthyPods.PodStatus, podname)
 			*StoreAllClusterPodList[cluster].SingleClusterHealthyPods.Number--
+			log.Println("delete a healthy pods " + podname + " in " + cluster)
 			return
 		}
 		delete(StoreAllClusterPodList[cluster].SingleClusterUnHealthyPods.PodStatus, podname)
 		*StoreAllClusterPodList[cluster].SingleClusterUnHealthyPods.Number--
+		log.Println("delete a unhealthy pods " + podname + " in " + cluster)
 		return
 	}
 
@@ -42,6 +44,7 @@ func PodModifyed(cluster string, poddetail map[string]k8s.Pod, podname string, e
 		if poddetail[podname].Status == "Running" || poddetail[podname].Status == "Succeeded" {
 			StoreAllClusterPodList[cluster].SingleClusterHealthyPods.PodStatus[podname] = poddetail[podname]
 			*StoreAllClusterPodList[cluster].SingleClusterHealthyPods.Number++
+			log.Println("add a healthy pod " + podname + "in " + cluster)
 			return
 		}
 		StoreAllClusterPodList[cluster].SingleClusterUnHealthyPods.PodStatus[podname] = poddetail[podname]
@@ -58,7 +61,7 @@ func PodModifyed(cluster string, poddetail map[string]k8s.Pod, podname string, e
 			StoreAllClusterPodList[cluster].SingleClusterHealthyPods.PodStatus[podname] = poddetail[podname]
 			*StoreAllClusterPodList[cluster].SingleClusterUnHealthyPods.Number--
 			*StoreAllClusterPodList[cluster].SingleClusterHealthyPods.Number++
-			log.Println("the Pod status cluster " + cluster + "was changed")
+			log.Println("change a unhealthy pod " + podname + "to healthy in " + cluster)
 			return
 		}
 	} else {
@@ -69,7 +72,7 @@ func PodModifyed(cluster string, poddetail map[string]k8s.Pod, podname string, e
 			StoreAllClusterPodList[cluster].SingleClusterUnHealthyPods.PodStatus[podname] = poddetail[podname]
 			*StoreAllClusterPodList[cluster].SingleClusterHealthyPods.Number--
 			*StoreAllClusterPodList[cluster].SingleClusterUnHealthyPods.Number++
-			log.Println("the Pod status cluster " + cluster + "was changed")
+			log.Println("change a healthy pod " + podname + "to unhealthy in " + cluster)
 			return
 		}
 	}
