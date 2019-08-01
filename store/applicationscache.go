218f13a2cc9786b05f56d79e8b34c93d4e727e32
lichang <changli@alauda.io>
2019-08-01 23:44:12 +0800

    test watch apps

--- store/applicationscache.go
+++ store/applicationscache.go
@@ -28,6 +28,7 @@ func AppModifyed(cluster string, poddetail map[string]k8s.Pod, podname string, e
 	if poddetail[podname].Apps != "" || poddetail[podname].Service_name != "" {
 		if poddetail[podname].Apps != "" {
 			thisL := poddetail[podname].Apps
+			log.Println(thisL)
 			Appplace, place, Appname := GetAppPlace(cluster, thisL, "A")
 			// log.Println(Appplace)
 			if Appplace == NilK8SPod {
